b18d7fb97f55d409caecf0d8633a95c287b67e04
Anneleen Peeters <anneleenpeeters02@gmail.com>
2020-06-12 22:39:31 +0200

    index opkuisen

---
+++ components/index/HomeHead.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import homeImage from '../images/home_main.jpg'
+import logoTitleImage from '../images/logo_title.png'
+
+const HomeHead = () => (
+    <Head>
+        <link rel="icon" href={logoTitleImage} type="image/icon type"/>
+        <title>La Garchère </title>
+        <meta name="title" content="La Garchère " />
+        <meta name="description" content="Ontdek dit la Garchère. Een gerenoveerde boerderij midden in Bourgondië. Ontspan en geniet. " />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="La Garchère " />
+        <meta property="og:description" content="Ontdek dit la Garchère. Een gerenoveerde boerderij midden in Bourgondië. Ontspan en geniet. " />
+        <meta property="og:image" content={homeImage} />
+    </Head>
+);  
+
+export default HomeHead;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,20 @@
-import Head from 'next/head'
-import homeImage from '../images/home_main.jpg'
-import logoTitleImage from '../images/logo_title.png'
-import HomeCom from '../components/HomeCom'
+import HomeCom from '../components/index/HomeCom'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import { parseCookies } from 'nookies'
+import HomeHead from '../components/index/HomeHead'
 
 export default function Home({jwt}) {
   return (
     <div className="container">
-      <Head>
-        <link rel="icon" href={logoTitleImage} type="image/icon type"/>
-        <title>La Garchère </title>
-        <meta name="title" content="La Garchère " />
-        <meta name="description" content="Ontdek dit la Garchère. Een gerenoveerde boerderij midden in Bourgondië. Ontspan en geniet. " />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="La Garchère " />
-        <meta property="og:description" content="Ontdek dit la Garchère. Een gerenoveerde boerderij midden in Bourgondië. Ontspan en geniet. " />
-        <meta property="og:image" content={homeImage} />
-      </Head>
+      <HomeHead />
       <div className="container">
-            <Nav jwt={jwt}/>
-            <div className="content">
-      <HomeCom />
+          <Nav jwt={jwt}/>
+          <div className="content">
+            <HomeCom />
+          </div>
+          <Footer/>
       </div>
-            <Footer/>
-        </div>
     </div>
   )
 }
