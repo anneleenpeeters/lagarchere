7a6c6d38d9662a49eb2a8eb245119ea74347a3d2
Anneleen Peeters <anneleenpeeters02@gmail.com>
2020-06-29 00:39:13 +0200

    deployen nog eens

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c0462209e375b1304a8a";
+/******/ 	var hotCurrentHash = "e6582e42ac1621ec0eab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
