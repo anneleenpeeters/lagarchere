2f071db7eb5fae966a2174449205479d5c134c7c
Anneleen Peeters <anneleenpeeters02@gmail.com>
2020-06-29 01:02:17 +0200

    test

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 module.exports = withImages(withCSS());
 
-module.exports = {
-    env: {
-        API_ENDPOINT: 'https://wdev.be/wdev_anneleen/eindwerk/api/',
-        IMAGE_PATH: 'https://wdev.be/wdev_anneleen/eindwerk/images/kamer/'
-    }
-}
+// module.exports = {
+//     env: {
+//         API_ENDPOINT: 'https://wdev.be/wdev_anneleen/eindwerk/api/',
+//         IMAGE_PATH: 'https://wdev.be/wdev_anneleen/eindwerk/images/kamer/'
+//     }
+// }
