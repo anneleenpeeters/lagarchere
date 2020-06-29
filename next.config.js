c4e2220c41e15050aa349756d501fa8ffe61cb57
Anneleen Peeters <anneleenpeeters02@gmail.com>
2020-06-29 09:40:13 +0200

    css aanpassing

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withImages(withCSS());
 module.exports = withImages(withCSS({
     env: {
         NEXT_PUBLIC_API_ENDPOINT: 'https://wdev.be/wdev_anneleen/eindwerk/api/',
-        NEXT_PUBLIC_MAGE_PATH: 'https://wdev.be/wdev_anneleen/eindwerk/images/kamer/',
+        NEXT_PUBLIC_IMAGE_PATH: 'https://wdev.be/wdev_anneleen/eindwerk/images/kamer/',
         NEXT_PUBLIC_MAP_TOKEN: "pk.eyJ1IjoiYW5uZWxlZW5wZWV0ZXJzIiwiYSI6ImNrYXd5d2QxYjAxNGgyeW5pNTJhaDdoMXEifQ.ls-zfu0zpOvHmm_xWvClfw"
     }
 
