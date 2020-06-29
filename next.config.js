5370efe98a60f5bd5a8dbf3daada89af1b480340
Anneleen Peeters <anneleenpeeters02@gmail.com>
2020-06-29 15:18:42 +0200

    deploy

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = withImages(withCSS({
     env: {
         NEXT_PUBLIC_API_ENDPOINT: 'https://wdev.be/wdev_anneleen/eindwerk/api/',
         NEXT_PUBLIC_IMAGE_PATH: 'https://wdev.be/wdev_anneleen/eindwerk/images/kamer/',
-        NEXT_PUBLIC_MAP_TOKEN: "pk.eyJ1IjoiYW5uZWxlZW5wZWV0ZXJzIiwiYSI6ImNrYXd5d2QxYjAxNGgyeW5pNTJhaDdoMXEifQ.ls-zfu0zpOvHmm_xWvClfw"
+        NEXT_PUBLIC_MAP_TOKEN: 'pk.eyJ1IjoiYW5uZWxlZW5wZWV0ZXJzIiwiYSI6ImNrYXd5d2QxYjAxNGgyeW5pNTJhaDdoMXEifQ.ls-zfu0zpOvHmm_xWvClfw'
     }
 }))
